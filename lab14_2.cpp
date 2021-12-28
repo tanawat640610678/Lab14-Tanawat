b43919ec63498541aabaeab379c2c8dcf40a8b7f
tanawat640610678 <thanawat_chai@cmu.ac.th>
2021-12-28 20:34:36 +0700

    Lab14_2.cpp Complete

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,39 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool D[][M]){
+
+    for(int a = 0 ; a < 72 ; a++) cout << "-";
+       cout << "\n";
+
+    for(int b = 0 ; b < N ; b++ ){
+        cout << "|";
+
+        for(int c = 0 ; c < M ; c++){
+            
+            if( D[b][c]){
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+        }
+        cout << "|" << "\n";
+    }
+    for(int i = 0; i<72 ;i++) cout << "-" ;
+      cout << "\n";
+}   
+
+void updateImage(bool P[][M],int s,int u,int v){
+
+       for(int j = 0 ; j < N ;j++){
+
+           for(int k = 0 ; k < M ; k++){
+
+               if( s >= 1 + sqrt(pow(j-u,2)+pow(k-v,2))){
+
+                   P[j][k] = 1;
+               }
+           }
+       }
+}
